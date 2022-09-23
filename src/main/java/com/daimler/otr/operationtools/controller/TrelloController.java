407e0521db17b431b122930e795b6b6cba867dd2
yangqian <qian.yang2@thoughtworks.com>
2022-09-23 10:02:03 +0800

    [yangqian] #N/A fix: remove try catch and add response entity.

--- src/main/java/com/daimler/otr/operationtools/controller/TrelloController.java
+++ src/main/java/com/daimler/otr/operationtools/controller/TrelloController.java
@@ -2,10 +2,10 @@ package com.daimler.otr.operationtools.controller;
 
 import com.daimler.otr.operationtools.dto.CreateTrelloCardDTO;
 import com.daimler.otr.operationtools.dto.TrelloCard;
-import com.daimler.otr.operationtools.exception.TrelloException;
 import com.daimler.otr.operationtools.service.TrelloService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,13 +20,7 @@ public class TrelloController {
     private final TrelloService trelloService;
 
     @PostMapping("/trello-cards")
-    public TrelloCard createTrelloCard(@RequestBody CreateTrelloCardDTO createTrelloCardDTO) {
-        try {
-            return trelloService.createTrelloCard(createTrelloCardDTO);
-        } catch (Exception e) {
-            log.info("create trello card failed, error message: {}", e.getMessage());
-            throw new TrelloException("create trello card failed!", e.getCause());
-        }
-
+    public ResponseEntity<TrelloCard> createTrelloCard(@RequestBody CreateTrelloCardDTO createTrelloCardDTO) {
+        return ResponseEntity.ok(trelloService.createTrelloCard(createTrelloCardDTO));
     }
 }
