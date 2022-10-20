810989424198bc2f447e1206c67faf2f9a68ad95
yangqian <qian.yang2@thoughtworks.com>
2022-10-20 15:06:10 +0800

    [yangqian] #N/A feat: add get customField and customFieldItem and update customFieldItem func.

--- src/main/java/com/thoughtworks/otr/snconnector/client/TrelloClient.java
+++ src/main/java/com/thoughtworks/otr/snconnector/client/TrelloClient.java
@@ -1,5 +1,12 @@
 package com.thoughtworks.otr.snconnector.client;
 
+import com.thoughtworks.otr.snconnector.configuration.TrelloConfiguration;
+import com.thoughtworks.otr.snconnector.constans.TrelloUrlConstant;
+import com.thoughtworks.otr.snconnector.dto.CustomField;
+import com.thoughtworks.otr.snconnector.dto.CustomFieldItem;
+import com.thoughtworks.otr.snconnector.dto.TrelloCard;
+import com.thoughtworks.otr.snconnector.dto.TrelloCardListDTO;
+import com.thoughtworks.otr.snconnector.exception.TrelloException;
 import com.julienvey.trello.Trello;
 import com.julienvey.trello.domain.Board;
 import com.julienvey.trello.domain.Card;
@@ -9,10 +16,6 @@ import com.julienvey.trello.domain.TList;
 import com.julienvey.trello.impl.TrelloImpl;
 import com.julienvey.trello.impl.TrelloUrl;
 import com.julienvey.trello.impl.http.JDKTrelloHttpClient;
-import com.thoughtworks.otr.snconnector.configuration.TrelloConfiguration;
-import com.thoughtworks.otr.snconnector.dto.TrelloCard;
-import com.thoughtworks.otr.snconnector.dto.TrelloCardListDTO;
-import com.thoughtworks.otr.snconnector.exception.TrelloException;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
@@ -20,8 +23,13 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
+
+import static java.util.Arrays.asList;
+import static java.util.Collections.emptyList;
 
 public class TrelloClient {
     private final TrelloConfiguration trelloConfiguration;
@@ -75,6 +83,36 @@ public class TrelloClient {
         return trelloApi.getBoardLabels(trelloConfiguration.getTrelloBoardId()).stream()
                         .filter(label -> StringUtils.isNotBlank(label.getName()))
                         .collect(Collectors.toList());
+    }
+
+    public List<CustomField> getBoardCustomFields() {
+        String url = TrelloUrl.API_URL + TrelloUrlConstant.GET_BOARD_CUSTOM_FIELDS;
+        URI fullUrl = getUriComponentsBuilder(url)
+                .buildAndExpand(trelloConfiguration.getTrelloBoardId())
+                .toUri();
+        CustomField[] customFields = restTemplate.getForObject(fullUrl, CustomField[].class);
+        return Objects.nonNull(customFields) ? asList(customFields) : emptyList();
+    }
+
+    public List<CustomFieldItem> getCardCustomFieldItems(String cardId) {
+        String url = TrelloUrl.API_URL + TrelloUrlConstant.GET_CARD_CUSTOM_FIELD_ITEMS;
+        URI fullUrl = getUriComponentsBuilder(url)
+                                        .buildAndExpand(cardId)
+                                        .toUri();
+        CustomFieldItem[] customFieldItems = restTemplate.getForObject(fullUrl, CustomFieldItem[].class);
+        return Objects.nonNull(customFieldItems) ? asList(customFieldItems) : emptyList();
+    }
+
+    public void updateCardCustomFieldItem(String cardId, String customFiledItemId, CustomFieldItem updateCustomFieldItem) {
+        String url = TrelloUrl.API_URL + TrelloUrlConstant.UPDATE_CARD_CUSTOM_FIELD_ITEM;
+        URI fullUrl = getUriComponentsBuilder(url)
+                                        .buildAndExpand(
+                                                Map.of(
+                                                        "cardId", cardId,
+                                                        "customFieldId", customFiledItemId))
+                                        .toUri();
+        restTemplate.put(fullUrl, updateCustomFieldItem);
+
     }
 
     private UriComponentsBuilder getUriComponentsBuilder(String url) {
---
+++ src/main/java/com/thoughtworks/otr/snconnector/constans/TrelloUrlConstant.java
@@ -0,0 +1,12 @@
+package com.thoughtworks.otr.snconnector.constans;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class TrelloUrlConstant {
+    public static final String GET_BOARD_CUSTOM_FIELDS = "/boards/{id}/customFields?";
+
+    public static final String GET_CARD_CUSTOM_FIELD_ITEMS = "/cards/{cardId}/customFieldItems?";
+    public static final String UPDATE_CARD_CUSTOM_FIELD_ITEM = "/cards/{cardId}/customField/{customFieldId}/item?";
+}
