fbd8457ddecacc3926bf401f630ac45ca2571124
yangqian <qian.yang2@thoughtworks.com>
2022-10-24 10:28:22 +0800

    [yangqian] #N/A feat: add trello card mapper.

--- src/main/java/com/thoughtworks/otr/snconnector/utils/mapper/TrelloCardMapper.java
+++ src/main/java/com/thoughtworks/otr/snconnector/utils/mapper/TrelloCardMapper.java
@@ -2,8 +2,13 @@ package com.thoughtworks.otr.snconnector.utils.mapper;
 
 import com.julienvey.trello.domain.Card;
 import com.thoughtworks.otr.snconnector.dto.TrelloCard;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 import org.mapstruct.factory.Mappers;
 
+@Mapper(
+        unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
 public interface TrelloCardMapper {
 
     TrelloCardMapper TRELLO_CARD_MAPPER = Mappers.getMapper(TrelloCardMapper.class);
