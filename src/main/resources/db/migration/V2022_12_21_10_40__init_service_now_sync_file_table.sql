8a6a96842f818ff2236031d199f21c1daced9714
yangqian <qian.yang2@thoughtworks.com>
2022-12-21 11:33:34 +0800

    [yangqian] #N/A feat: create attachment for card.

---
+++ src/main/resources/db/migration/V2022_12_21_10_40__init_service_now_sync_file_table.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS `service_now_sync_file`(
+    `id` bigint(10) NOT NULL AUTO_INCREMENT,
+    `ticket` varchar(100) NOT NULL,
+    `name` TEXT NOT NULL,
+    `url_link` VARCHAR(500) NOT NULL,
+    `created_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`service_now_sync_data_id`) REFERENCES service_now_sync_data(`id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE INDEX `idx_ticket` on `service_now_sync_data_file`(`ticket`);
